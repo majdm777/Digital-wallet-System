61faafca90927dc7ebfb56322bafff27aeb947be
Majd Maatouk <maatoukmajd.520@gmail.com>
2025-12-09 21:05:07 +0200

    majd v1.3.2

---
+++ Maindata.php
@@ -0,0 +1,12 @@
+<?php
+try {
+    $db = new mysqli('localhost', 'root', '', 'wallet_db');
+} catch (\Exception $e) {
+    // Die with a connection error message
+    die("<h1>Database Connection Failed!</h1><p>Error: " . $e->getMessage() . "</p>");
+}
+
+
+
+
+?>
